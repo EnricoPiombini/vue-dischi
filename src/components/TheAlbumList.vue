a00df4728ca6c1a7be41b25991808bbdd4cdf93e
EnricoPiombini <piombini.enrico@gmail.com>
2022-06-25 10:53:30 +0200

    Immagini stampate

---
+++ src/components/TheAlbumList.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container">
+
+
+        <div class="row">
+            <div class="col-3" v-for="disc in albumInfo" :key="disc.author">
+                <TheSingleDisc :author-name="disc">
+
+                </TheSingleDisc>
+            </div>
+        </div>
+    </div>
+
+
+
+
+</template>
+
+
+<script>
+import axios from "axios";
+import TheSingleDisc from "./TheSingleDisc.vue";
+
+
+export default {
+    name: "TheDiscs",
+    components: { TheSingleDisc },
+    data() {
+        return {
+            apiUrl: "https://flynn.boolean.careers/exercises/api/array/music",
+            albumInfo: [],
+        }
+    },
+    methods: {
+        fetchMusicList() {
+            axios.get(this.apiUrl).then((response) => {
+                this.albumInfo.push(...response.data.response)
+
+
+
+            });
+
+        },
+    },
+    mounted() {
+        this.fetchMusicList()
+    }
+}
+
+
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/TheSingleDisc.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="card">
+            <div class="card-body">
+                <img :src="authorName.poster" alt="" class="w-100">
+                <h5> {{ authorName.title }} </h5>
+                <p>{{ authorName.author }} {{ authorName.year }}  </p>
+                
+            </div>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+export default {
+    props: {
+        authorName: Object,
+    },
+    name: "TheDisc",
+
+}
+
+</script>
+
+
+<style>
+</style>
